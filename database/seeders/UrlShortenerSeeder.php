15878901712e36e83c49680285992aa9a3582471
Yohanny Lugo <78890742+dev-yohannylugo@users.noreply.github.com>
2024-11-18 11:19:43 -0400

    add XSS attack

--- database/seeders/UrlShortenerSeeder.php
+++ database/seeders/UrlShortenerSeeder.php
@@ -12,6 +12,14 @@ class UrlShortenerSeeder extends Seeder
      */
     public function run(): void
     {
-        UrlShortener::factory(10)->create();
+        UrlShortener::factory(8)->create();
+        UrlShortener::factory()->create([
+            'code' =>  UrlShortener::generateShortCode(),
+            'original_url' => 'script>alert("¡Esto es un ataque!");</script><p>Contenido seguro</p>',
+        ]);
+        UrlShortener::factory()->create([
+            'code' =>  'script>alert("¡Esto es otro ataque!");</script><p>Contenido seguro</p>',
+            'original_url' => fake()->url(),
+        ]);
     }
 }
